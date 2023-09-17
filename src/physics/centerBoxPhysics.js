3e6de64bd5d13f5f9c8c6b0bf0d74426351db3b0
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-09-17 14:48:40 +0100

    Position centerbox on the floor

--- src/physics/centerBoxPhysics.js
+++ src/physics/centerBoxPhysics.js
@@ -1,9 +1,9 @@
 import { Body, Box, Vec3 } from "cannon-es";
 
 const centerBoxBody = new Body({
-  shape: new Box(new Vec3(50, 50, 50)),
-  mass: 1,
+	shape: new Box(new Vec3(50, 50, 50)),
+	mass: 1,
 });
-centerBoxBody.position.set(0, 400, 0);
+centerBoxBody.position.set(0, 55, 0);
 
 export default centerBoxBody;
